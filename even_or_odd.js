d1cdf7475ace5299460fd9f96495cc49855f75a0
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-04-30 15:34:35 -0600

    even or odd finished

--- even_or_odd.js
+++ even_or_odd.js
@@ -1,3 +1,7 @@
+
+// Create a function that takes an integer as an argument and returns "Even"
+// for even numbers or "Odd" for odd numbers.
+
 function evenOrOdd(n){
   if(n % 2) {
     return 'odd'
