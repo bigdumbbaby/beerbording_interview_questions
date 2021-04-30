328ec73c296332f762d58d8f3d59b340a0ce2b49
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-04-30 15:33:54 -0600

    even or odd

---
+++ even_or_odd.js
@@ -0,0 +1,14 @@
+function evenOrOdd(n){
+  if(n % 2) {
+    return 'odd'
+  } else {
+    return 'even'
+  }
+}
+
+console.log(evenOrOdd(8))
+console.log(evenOrOdd(11))
+console.log(evenOrOdd(2))
+console.log(evenOrOdd(1))
+console.log(evenOrOdd(20))
+console.log(evenOrOdd(25))
