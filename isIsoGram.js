92df35ae0489d6ddd9ff874945ec296e91fc1a95
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-07-12 12:46:46 -0600

    isIsoGram created

---
+++ isIsoGram.js
@@ -0,0 +1,11 @@
+function isIsoGrams(str){
+  for(let i=0; i<str.length; i++){
+    if(str[i] === str[i+1]){
+      return false
+    }
+  }
+  return true
+}
+
+console.log(isIsoGrams('hello'))
+console.log(isIsoGrams('rest'))
